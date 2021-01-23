56e50aa3ac185048ed92a35838325d5bd9624e44
tahiry-dev <tahirycontact@gmail.com>
2021-01-23 09:05:58 +0300

    add about section

---
+++ js/skills-bar.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('.html').css('width', '98%');
+  $('.css').css('width', '70%');
+  $('.lesssass').css('width', '98%');
+  $('.jquery').css('width', '80%');
+  $('.javascript').css('width', '70%');
+  $('.bootstrap').css('width', '60%');
+  $('.ruby').css('width', '90%');
+  $('.rails').css('width', '90%');
+  $('.react').css('width', '95%');
+  $('.python').css('width', '90%');
+  $('.java').css('width', '40%');
+});
